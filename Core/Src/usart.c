cb1084e19990cb42295ee302c8cd1eaa4b437316
czt <2300739265@qq.com>
2026-09-03 09:27:00 +0800

    Refactor code structure and optimize performance across multiple modules

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -84,7 +84,7 @@ void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 9600;
+  huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
@@ -474,4 +474,3 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
-
